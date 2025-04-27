e098db3b6ac8c77a22318b7a211f65517d43442d
Wenwei-Zhou <wenweizhou0@gmail.com>
2025-04-28 00:30:49 +1000

    completed_course
    completed course ASP.NET Web API

---
+++ MyWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebApi.Models;
+
+namespace MyWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
